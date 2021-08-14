bfeac81573bfef7f40ff289b39ad8413ee2c2083
parssa-airborneapp <parssak@gmail.com>
2021-08-14 19:18:25 -0400

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import Editor from "@monaco-editor/react";
 
 function App() {
   return (
-    <div className="bg-black h-screen">
-      <Editor
-        height="90vh"
-        theme="vs-dark"
-        defaultLanguage="typescript"
-        defaultValue="// some comment"
-      />
-    </div>
+    <main className="w-screen h-screen">
+      <nav className="bg-red-50 p-5"> nav</nav>
+      <section className="w-full h-full grid grid-cols-2">
+        <div className="bg-blue-300 h-full"></div>
+        <div className="bg-red-300 h-full">
+          <Editor
+            height="100%"
+            theme="vs-dark"
+            defaultLanguage="typescript"
+            defaultValue="// some comment"
+          />
+        </div>
+      </section>
+      
+    </main>
   );
 }
 
