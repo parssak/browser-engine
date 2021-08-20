1374da15ef929872f9b9fe4e05b76cf2d6939848
parssa-airborneapp <parssak@gmail.com>
2021-08-20 17:04:36 -0400

    cleaning files

--- src/ui/base/BrowserEngine.tsx
+++ src/ui/base/BrowserEngine.tsx
@@ -1,10 +1,8 @@
-import { Resizable } from 're-resizable'
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 import useEditor from '../../state/editor/useEditor'
 import useScene from '../../state/scene/useScene'
 import Controls from '../components/Controls'
 import TreeViewPanel from '../components/TreeViewPanel'
-
 
 export default function BrowserEngine(): ReactElement {
   const { createEntity, selectedEntity } = useScene()
@@ -18,8 +16,6 @@ export default function BrowserEngine(): ReactElement {
       </nav>
       <section className="w-full h-full grid" style={{ gridTemplateColumns: '1fr 14rem' }}>
         {renderElement && (<div className="bg-gray-600" id="scene" ref={renderElement} />)}
-        {/* <Resizable className="border-r-4 border-gray-700 z-20" minWidth="30vw" maxWidth="70vw" onResize={() => SceneManager?.cameraManager.HandleResize()}>
-        </Resizable> */}
        <TreeViewPanel />
       </section>
       {selectedEntity && <Controls />}
