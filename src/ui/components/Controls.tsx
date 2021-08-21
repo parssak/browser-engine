ec9e94ce1ea8fbd034a968d6666325266446e524
parssak <parssak@gmail.com>
2021-08-21 11:13:29 -0400

    updating fields correctly

--- src/ui/components/Controls.tsx
+++ src/ui/components/Controls.tsx
@@ -1,4 +1,5 @@
 import { folder, Leva,  useControls } from 'leva'
+import { useEffect } from 'react';
 import useScene from '../../state/scene/useScene';
 
 export default function Controls() {
@@ -20,8 +21,24 @@ export default function Controls() {
     return Object.fromEntries(componentControls);
   }
 
-  const controls = useControls(getControls as any, [selectedEntity]);
-  console.debug({ controls });
+  const [values, set]: any = useControls(getControls as any, [selectedEntity]);
+
+  useEffect(() => {
+    const updateComponentFields = (entity: Engine.EntityProps) => {
+      console.debug(entity.components, values, set);
+      const flattenedPropFields: Record<string, Engine.ComponentPropType> = {};
+      Object.values(entity.components).forEach((component) => {
+        const [fieldName, fieldValue] = Object.entries(component)[0];
+        flattenedPropFields[fieldName] = fieldValue;
+      });
+      set(flattenedPropFields);
+    }
+    if (selectedEntity) updateComponentFields(selectedEntity);
+  }, [selectedEntity])
+
+  useEffect(() => {
+    console.debug('values changed', values);
+  }, [values])
 
   return (
     <div className="bg-indigo-500 space-y-1">
