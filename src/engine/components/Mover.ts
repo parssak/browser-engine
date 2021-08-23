67e375b15e2459a25c6b8a0177bdc4f988e4d161
parssa-airborneapp <parssak@gmail.com>
2021-08-23 11:36:11 -0400

    mover working

--- src/engine/components/Mover.ts
+++ src/engine/components/Mover.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import Component from '../core/Component';
 import Entity from '../core/Entity';
+import Transform from './Transform';
 
 interface IMoverProps {
   speed?: number;
@@ -9,13 +10,16 @@ export default class Mover extends Component {
   name = "mover";
   _entity!: Entity;
   speed: number = 0.1;
+  transform!: Transform;
 
   init(entity: Entity, props: IMoverProps) {
     this._entity = entity;
     this.speed = props?.speed ?? 0.1;
+    this.transform = this._entity.components['transform'] as Transform;
   }
 
   update() {
-    
+    console.debug('updating mover');
+    this.transform.position.x += this.speed;
   }
 }
