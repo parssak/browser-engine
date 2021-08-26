633b30eb3cd2d5e85799597294175d99bcbf661c
parssa-airborneapp <parssak@gmail.com>
2021-08-26 15:08:23 -0400

    cleaning up editor

--- src/ui/base/BrowserEngine.tsx
+++ src/ui/base/BrowserEngine.tsx
@@ -10,24 +10,71 @@ import ScriptPanel from '../components/ScriptPanel'
 export default function BrowserEngine(): ReactElement {
   const { createEntity, selectedEntity } = useScene()
   const { renderElement, toggleRun, isRunning, handleClickScene } = useEditor()
-  const { createScript,  selectedScript, loadScript, saveScript } = useScripts();
-  
+  const { createScript, selectedScript, loadScript, saveScript } = useScripts();
+
   const [scriptBody, setScriptBody] = useState('');
-  
+
   const handleEditorChange = (newValue: string | undefined) => {
     setScriptBody(newValue ?? '')
+  }
+
+  const handleCreateScript = () => {
+    createScript('Component')
+  }
+
+  const handleSaveScript = () => {
+    if (selectedScript) {
+      saveScript({ ...selectedScript, content: scriptBody })
+    }
+  }
+
+  const handleCloseScript = () => {
+    loadScript('');
   }
 
   return (
     <main className="w-screen h-screen overflow-hidden">
       <div className={`h-0.5 transition bg-red-600 ${isRunning && 'bg-green-500'}`}></div>
-      <nav className="bg-gray-900 p-2 space-x-2">
-        <button onClick={toggleRun} className="primary">{isRunning ? 'Stop' : 'Run'} Scene</button>
-        <button onClick={createEntity} className="secondary">Add Entity</button>
-        <button onClick={() => createScript('foo')} className="secondary">Add Script</button>
+      <nav className="bg-gray-900 p-2 flex w-full">
+        <div className="space-x-2">
+          <button onClick={toggleRun} className="primary">{isRunning ? 'Stop' : 'Run'} Scene</button>
+          <button onClick={createEntity} className="secondary">Add Entity</button>
+          <button onClick={handleCreateScript} className="secondary">Add Script</button>
+        </div>
+        {
+          selectedScript &&
+          (
+            <div className="space-x-2 ml-auto">
+              <button
+                className="secondary"
+                onClick={handleCloseScript}
+              >Close Editor</button>
+              <button
+                onClick={handleSaveScript}
+                className="submit">Save script</button>
+            </div>
+          )
+        }
       </nav>
       <section className="w-full h-full grid" style={{ gridTemplateColumns: '1fr 17rem' }}>
-        {renderElement && (<div className="bg-gray-600" id="scene" ref={renderElement} onClick={(e) => handleClickScene(e)} />)}
+        <div className="h-full w-full grid relative">
+          {renderElement && (<div className="bg-gray-600" id="scene" ref={renderElement} onClick={(e) => handleClickScene(e)} />)}
+          {
+            selectedScript && (
+              <>
+                <div className="absolute h-full w-full" key={selectedScript.id}>
+                  <Editor
+                    height="100%"
+                    width="100%"
+                    defaultLanguage="javascript"
+                    defaultValue={selectedScript.content}
+                    theme="vs-dark"
+                    onChange={handleEditorChange} />
+                </div>
+              </>
+            )
+          }
+        </div>
         <div className="grid grid-rows-5 gap-0 5">
           <TreeViewPanel />
           <div className="row-span-2">
@@ -38,13 +85,13 @@ export default function BrowserEngine(): ReactElement {
           </div>
         </div>
       </section>
-      {selectedScript && (<section className="fixed inset-0 grid place-items-center" onClick={() => loadScript('')}>
+      {/* {selectedScript && (<section className="fixed inset-0 grid place-items-center" onClick={() => loadScript('')}>
         <div
           onClick={e => {
             e.stopPropagation();
             saveScript({ ...selectedScript, content: scriptBody });
           }}
-          className="absolute top-10 right-10 py-2 px-4 cursor-pointer rounded-md bg-indigo-600 text-white">
+          className="absolute top-10 right-10 py-2 px-4 cursor-pointer rounded-md bg-green-600 text-white">
           Save
         </div>
         <div className="bg-black monaco-editor" style={{ width: '60vw' }} onClick={e => e.stopPropagation()}>
@@ -55,7 +102,7 @@ export default function BrowserEngine(): ReactElement {
             theme="vs-dark"
             onChange={handleEditorChange} />
         </div>
-      </section>)}
+      </section>)} */}
     </main>
   )
 }
