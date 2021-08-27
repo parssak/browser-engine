c6fc8aea03a75346cd0dd9121016d814786fcbdf
parssa-airborneapp <parssak@gmail.com>
2021-08-27 12:02:24 -0400

    smaller buttons

--- src/state/scripts/useScripts.tsx
+++ src/state/scripts/useScripts.tsx
@@ -1,27 +1,39 @@
-
-import { useContext, useState, } from "react";
-import { generateNewScript } from "../../utils/script.utils";
-import { ScriptContext } from "./ScriptContext";
+import { useContext, useEffect, useState } from "react"
+import { generateNewScript } from "../../utils/script.utils"
+import { ScriptContext } from "./ScriptContext"
 
 const useScripts = () => {
-  const { scripts, setScripts, selectedScript, setSelectedScript, compileScripts } = useContext(ScriptContext);
-  const [scriptBody, setScriptBody] = useState<string>(selectedScript?.content ?? "")
+  const {
+    scripts,
+    setScripts,
+    selectedScript,
+    setSelectedScript,
+    compileScripts,
+    scriptBody,
+    setScriptBody,
+  } = useContext(ScriptContext)
 
   const loadScript = (scriptName: string) => {
-    setSelectedScript(scripts[scriptName]);
-  };
-  
+    const foundScript = scripts[scriptName]
+    setSelectedScript(foundScript)
+    setScriptBody(foundScript?.content ?? "")
+  }
+
   const saveScript = (script: Engine.Script) => {
     const foundScript = Object.values(scripts).find((s) => s.id === script.id)
-    if (!foundScript) return;
-    foundScript.content = script.content;
-    compileScripts();
-  };
-  
+    if (!foundScript) return
+    foundScript.content = scriptBody;
+    compileScripts()
+  }
+
   const createScript = (name: string) => {
-    const newScript = generateNewScript(name);
-    setScripts({...scripts, [newScript.name]: newScript});
-  };
+    const newScript = generateNewScript(name)
+    setScripts({ ...scripts, [newScript.name]: newScript })
+  }
+
+  useEffect(() => {
+    console.debug("script body is", scriptBody)
+  }, [scriptBody])
 
   return {
     scripts: Object.values(scripts),
@@ -33,7 +45,7 @@ const useScripts = () => {
     loadScript,
     saveScript,
     createScript,
-  };
+  }
 }
 
-export default useScripts;
+export default useScripts
