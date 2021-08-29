d8f38863f4221dc7aa977a817df7f276d21c6338
parssa-airborneapp <parssak@gmail.com>
2021-08-29 06:32:41 -0400

    moved all script compilation logic to own class

--- src/state/scripts/ScriptContext.tsx
+++ src/state/scripts/ScriptContext.tsx
@@ -1,10 +1,6 @@
-import * as three from "three"
-import { Component, ReactElement, useEffect, useState } from "react"
+import {  ReactElement, useEffect, useState } from "react"
 import { createContext } from "react"
-import ComponentManager from "../../engine/core/ComponentManager"
-import { formatScriptString, injectInitSection } from "../../utils/script.utils"
-import SceneManager from "../../engine/core/SceneManager"
-import { generateNewEntity } from "../../utils/entity.utils"
+import ScriptCompiler from "../../utils/ScriptCompiler"
 
 interface IScriptContext {
   scripts: Record<string, Engine.Script>
@@ -38,41 +34,7 @@ export const ScriptProvider = ({
   const [scriptBody, setScriptBody] = useState(selectedScript?.content ?? "")
 
   const compileScripts = () => {
-    
-    const compileComponentScript = (script: Engine.Script) => {
-      // References to be used when running eval
-      const THREE = three                           
-      const Instantiate = SceneManager.instance.buildEntity.bind(SceneManager.instance)
-      const CreateEntity = generateNewEntity
-
-      const scriptCopy = `${script.content}`
-      const removePrefix = scriptCopy.substring(
-        scriptCopy.indexOf("// <public>") + 11
-      )
-      let pureProps = removePrefix.substring(0, removePrefix.indexOf("// </public>"))
-      pureProps = pureProps.replaceAll(";", ",")
-      pureProps = pureProps.replaceAll("=", ":")
-      let props: Record<string, Engine.ComponentType> = {}
-      if (`${pureProps}`.replace(/\s/g, "").length) {
-        pureProps = `{${pureProps}}`
-        props = eval(`(${pureProps})`) // ! <-- Eval
-      }
-      let formattedScript = formatScriptString(script)
-      formattedScript = injectInitSection(formattedScript, props)
-      const NewComponent: any = eval(`(${formattedScript})`) // ! <-- Eval
-      Object.setPrototypeOf(NewComponent, Component)
-      ComponentManager.instance.registerComponent(script.name, NewComponent, props)
-    }
-
-    Object.values(scripts).forEach((script) => {
-      try {
-        if (script.type === "component") {
-          compileComponentScript(script);
-        } 
-      } catch (err) {
-        console.error(err)
-      }
-    })
+    ScriptCompiler.CompileScripts(Object.values(scripts));
   }
 
   useEffect(() => {
---
+++ src/utils/ScriptCompiler.ts
@@ -0,0 +1,44 @@
+import * as three from "three"
+import Component from "../engine/core/Component"
+import ComponentManager from "../engine/core/ComponentManager"
+import SceneManager from "../engine/core/SceneManager"
+import { generateNewEntity } from "./entity.utils"
+import { formatScriptString, injectInitSection } from "./script.utils"
+// Helper for script compilation
+
+export default class ScriptCompiler {
+  public static CompileScripts(scripts: Engine.Script[]) {
+    const compileComponentScript = (script: Engine.Script) => {
+      // References to be used when running eval
+      const THREE = three
+      const Instantiate = SceneManager.instance.buildEntity.bind(SceneManager.instance)
+      const CreateEntity = generateNewEntity
+
+      const scriptCopy = `${script.content}`
+      const removePrefix = scriptCopy.substring(scriptCopy.indexOf("// <public>") + 11)
+      let pureProps = removePrefix.substring(0, removePrefix.indexOf("// </public>"))
+      pureProps = pureProps.replaceAll(";", ",")
+      pureProps = pureProps.replaceAll("=", ":")
+      let props: Record<string, Engine.ComponentType> = {}
+      if (`${pureProps}`.replace(/\s/g, "").length) {
+        pureProps = `{${pureProps}}`
+        props = eval(`(${pureProps})`) // ! <-- Eval
+      }
+      let formattedScript = formatScriptString(script)
+      formattedScript = injectInitSection(formattedScript, props)
+      const NewComponent: any = eval(`(${formattedScript})`) // ! <-- Eval
+      Object.setPrototypeOf(NewComponent, Component)
+      ComponentManager.instance.registerComponent(script.name, NewComponent, props)
+    }
+
+    Object.values(scripts).forEach((script) => {
+      try {
+        if (script.type === "component") {
+          compileComponentScript(script)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    })
+  }
+}
